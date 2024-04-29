2823089970f7d58d943d224ed6dfa1e280413b05
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2024-04-30 01:39:40 +0800

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector2 direction;
+    private Rigidbody2D playerRb;
+    [SerializeField] float SPEED;
+    [SerializeField] float STOP_FRICTION;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+
+        if (direction.sqrMagnitude > 0)
+        {
+            playerRb.velocity = direction * SPEED * Time.deltaTime;
+        }
+        else
+        {
+        
+        }
+
+
+    }
+
+    private void GetInput()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+        direction.Normalize();
+    }
+
+}
