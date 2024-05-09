840da3c6b3b2d99dc25f7f3b18029b3bf0221b6b
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2024-05-09 22:33:40 +0800

    spawner

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] GameObject meleeEnemyPrefab;
+    GameObject rangedEnemyPrefab;
+    private float spawnRate;
+    private float meleeSpawnChance;
+    private float rangedSpawnChance;
+    private float timeToNextSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
+        meleeSpawnChance = 0.5f;
+        spawnRate = 1.0f;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timeToNextSpawn <= 0)
+        {
+            float roll = Random.value;
+            if (roll <= meleeSpawnChance)
+            {
+                GameObject.Instantiate(meleeEnemyPrefab, transform.position, Quaternion.identity);
+            }
+            timeToNextSpawn = spawnRate;
+        }
+
+        timeToNextSpawn -= Time.deltaTime;
+
         
     }
 }
