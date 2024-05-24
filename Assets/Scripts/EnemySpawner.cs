6544604fb731f40ae5c17ee97d1298b0b80eece1
Michael Anthony Bitoon <mbitoon@up.edu.ph>
2024-05-24 17:23:51 +0800

    update: difficulty increase, enemy spawning system

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -9,14 +9,23 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float spawnRate;
     [SerializeField] private float meleeSpawnChance;
     [SerializeField] private float rangedSpawnChance;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float spawnDistance;
     private float timeToNextSpawn;
     private bool active = true;
+
+    private int[] expThreshold = { 100, 300, 400, 700, 1100 };
+
+    private int spawnLevel = 0;
+    private int totalExp = 0;
 
     private void Start()
     {
         GameEvents.current.PauseGame.AddListener(Stop);
         GameEvents.current.ResumeGame.AddListener(Resume);
+        GameEvents.current.EnemyDeath.AddListener(OnEnemyDeath);
     }
+
 
     void Stop()
     {
@@ -28,21 +37,45 @@ public class EnemySpawner : MonoBehaviour
         active = true;
     }
 
+    void OnEnemyDeath(int exp)
+    {
+        totalExp += exp;
+        if (totalExp > expThreshold[spawnLevel])
+        {
+            spawnLevel += 1;
+            IncreaseDifficulty();
+        }
+    }
+
+    void IncreaseDifficulty()
+    {
+        Debug.Log("Difficulty Increased");
+        spawnRate *= 1.5f;
+
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        if (playerTransform == null)
+        {
+            active = false;
+        }
         if (!active) return;
+
+        Vector3 randomPos = GetRandomOffScreenPosition();
         if (timeToNextSpawn <= 0)
         {
             float roll = Random.value;
             if (roll <= rangedSpawnChance)
             {
-                GameObject.Instantiate(rangedEnemyPrefab, transform.position, Quaternion.identity);
+                GameObject.Instantiate(rangedEnemyPrefab, randomPos, Quaternion.identity);
             }
             if (roll <= meleeSpawnChance)
             {
-                GameObject.Instantiate(meleeEnemyPrefab, transform.position, Quaternion.identity);
+                GameObject.Instantiate(meleeEnemyPrefab, randomPos, Quaternion.identity);
             }
 
             timeToNextSpawn = spawnRate;
@@ -50,6 +83,23 @@ public class EnemySpawner : MonoBehaviour
 
         timeToNextSpawn -= Time.deltaTime;
 
-        
+
     }
+
+    Vector3 GetRandomOffScreenPosition()
+    {
+        Vector3 playerPosition = playerTransform.position;
+        float raandomAngle = Random.Range(0f, 360f);
+        float spawnDistanceX = spawnDistance + Random.Range(0f, 5f);
+        float spawnDistanceY = spawnDistance + Random.Range(0f, 5f);
+
+        Vector3 offset = new Vector3(
+            Mathf.Cos(raandomAngle) * spawnDistanceX,
+            Mathf.Sin(raandomAngle) * spawnDistanceY, 0
+        );
+
+        return playerPosition + offset;
+
+    }
+
 }
